183615d96d79e7947d98b23433ad4a7a386faae2
MinjeongKong <kong1301@ajou.ac.kr>
2022-01-07 22:30:50 +0900

    git test

---
+++ src/test/java/com/healthtrainer/htserver/HtServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthtrainer.htserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
